c391132e261faf9d6a6eca79c1bd75aa8e078c88
IsIANVL <caprequin@gmail.com>
2025-03-02 17:55:01 -0600

    feat: se agregan las rutas de los controladores

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,43 @@
 import { Router } from "express";
+
+/** CREATE */
 import { createProducto } from "../controllers/productos/create/createProducto";
+
+/** READ */
+import { getProductos } from "../controllers/productos/read/getProductos";
+import { getProductoById } from "../controllers/productos/read/getProductoById";
+import { getProductosFilters } from "../controllers/productos/read/getProductosFilters";
+
+
+/** UPDATE */
+import { updateProducto } from "../controllers/productos/update/updateProducto";
+
+/** DELETE */
+import { deleteProducto } from "../controllers/productos/delete/deleteProducto";
 
 
 const router = Router();
 
-
-/** RUTA NUEVA: crear producto **/
+// Crear producto
+// POST /api/productos
 router.post("/productos", createProducto);
 
+// Leer todos los productos (GET simple)
+router.get("/productos", getProductos);
+
+// Leer productos con filtros
+router.post("/productos/filters", getProductosFilters);
+
+// Leer un producto por ID
+// GET /api/productos/:id
+router.get("/productos/:id", getProductoById);
+
+// Actualizar un producto
+// PUT /api/productos/:id
+router.put("/productos/:id", updateProducto);
+
+// Eliminar un producto
+// DELETE /api/productos/:id
+router.delete("/productos/:id", deleteProducto);
 
 export default router;
