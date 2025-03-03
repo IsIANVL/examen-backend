907c8410eea2a6c82f32eddcf7e9f0e45c170188
IsIANVL <caprequin@gmail.com>
2025-03-02 18:04:34 -0600

    feat: se agregan las rutas de los controladores v3

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-// Declaración global para evitar múltiples instancias de Prisma en desarrollo
+
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
-// Crear una nueva instancia de Prisma o usar la global existente
-export const db = globalThis.prisma || new PrismaClient();
-// Si no estamos en producción, mantener una instancia global para hot-reloading en desarrollo
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+export const db: PrismaClient =
+  global.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = db;
+}
